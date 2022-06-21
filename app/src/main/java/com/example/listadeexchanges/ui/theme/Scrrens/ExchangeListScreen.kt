376858496979b17c97c3309800e97c7588015e6d
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-06-21 01:16:11 -0400

    Ajustando title del scaffold

--- app/src/main/java/com/example/listadeexchanges/ui/theme/Scrrens/ExchangeListScreen.kt
+++ app/src/main/java/com/example/listadeexchanges/ui/theme/Scrrens/ExchangeListScreen.kt
@@ -26,7 +26,7 @@ fun ExchangeListScreen(
         topBar = {
             TopAppBar(
                 title = {
-                    Text("Lista de Exchanges")
+                    Text("List of Exchanges")
                 }
             )
         }
